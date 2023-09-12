306991b9757c753b0a31b795f665f9f31801641e
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-09-12 12:09:11 +0530

    12-aug

---
+++ 13. DOM/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,10 @@
+// this is to generate a random number to render associated dice image
+
+
+randomNumber1 = Math.floor((Math.random()*6)+1);
+randomNumber2 = Math.floor((Math.random()*6)+1);
+
+
+document.querySelectorAll(".dice img")[0].setAttribute(src, ")
+// console.log(randomNumber)
+
