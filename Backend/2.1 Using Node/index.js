b9b345c4bc10890a50e92e689657a2e7fbab49aa
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-11-10 23:13:03 +0530

    10-nov

---
+++ Backend/2.1 Using Node/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.writeFile("message.txt", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
+
+fs.readFile("message.txt", 'utf8', (err, data) => {
+    if(err) {
+        return console.log(err);
+
+    }
+    console.log(data);
+})
