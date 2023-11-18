a1f562fad347e14f1b044d407510742f1ae1a33e
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-11-18 16:30:47 +0530

    18-novf

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,28 @@
+import bodyParser from "body-parser";
 import express from "express";
+import morgan from "morgan";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+// impor bodyParser
+
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("tiny"));
+
+app.post('/submit', (req, res) => {
+  var result = req.body['street'] + req.body['pet'];
+  res.send(`<h1>the name of the band is ${result}</h1>`)
+  console.log(req.body);
+})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
