6b33de2fca932f0af27a8ef3770d57221111dfc4
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-11-18 16:41:08 +0530

    18-nov

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -11,13 +11,20 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+// use your own middleware
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("tiny"));
 
+function bandNameGen(req, res, next) {
+  console.log(req.body);
+  var bandName = req.body['street'] + req.body['pet'];
+  next();
+}
+app.use(bandNameGen);
+
 app.post('/submit', (req, res) => {
-  var result = req.body['street'] + req.body['pet'];
-  res.send(`<h1>the name of the band is ${result}</h1>`)
+  res.send(`<h1>the name of the band is ${bandName}</h1>`)
   console.log(req.body);
 })
 app.get('/', (req, res) => {
