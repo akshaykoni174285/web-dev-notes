14b08224f5e40eb8836ed88e35ddffede3be54b7
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-11-30 22:09:40 +0530

    30-nov

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -1,14 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs")
 
-app.post("/submit", (req, res) => {});
+});
+
+app.post("/submit", (req, res) => { 
+  const wordlen = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", {
+    wordlen: wordlen,
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
