4b2ec918646aeeb18a503f7df1eb98883e71e68f
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-01-02 00:26:43 +0530

    added delete

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -100,13 +100,21 @@ app.patch("/jokes/:id", (req, res) => {
 
 //7. DELETE Specific joke
 
-app.delete("jokes/:id", (req, res)=>{
+app.delete("/jokes/:id", (req, res)=>{
   const id = parseInt(req.params.id)
-  const jokeid = jokes.findIndex((joke) => joke.id === id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    // jokes.remove(jokes[jokeid]);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+
   
-  res.json(jokes[jokeid])
-  console.log(jokes[jokeid])
-  jokes.remove(jokeid);
 
 
 })
