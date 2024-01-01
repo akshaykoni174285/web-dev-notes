52c18d97ed727154ee73d4ad5d047587e8b6bfc8
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-01-01 23:42:02 +0530

    added patch and put

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -38,7 +38,7 @@ app.get('/jokes/:id', (req, res) => {
 
 app.get('/filter/:type', (req, res) => {
   const paraString = req.params.type;
-  const foundjoke = jokes.filter((joke) => joke.jokeType === paraString);
+  const foundjoke = jokes.filter((joke) => joke.jokeType.toLocaleLowerCase() === paraString.toLocaleLowerCase());
   if (foundjoke){
     res.json(foundjoke)
   }
@@ -51,11 +51,65 @@ app.get('/filter/:type', (req, res) => {
 
 //4. POST a new joke
 
-//5. PUT a joke
+// Post a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
+
+//Put a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = replacementJoke;
+  // console.log(jokes);
+  res.json(replacementJoke);
+});
+
+
+
 
 //6. PATCH a joke
+//Patch a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
+
+app.delete("jokes/:id", (req, res)=>{
+  const id = parseInt(req.params.id)
+  const jokeid = jokes.findIndex((joke) => joke.id === id);
+  
+  res.json(jokes[jokeid])
+  console.log(jokes[jokeid])
+  jokes.remove(jokeid);
+
+
+})
 
 //8. DELETE All jokes
 
